2e5c3e22efb3756d008e5bc30960ccec3a2a4589
sahil <maleksahil9315@gmail.com>
2024-12-01 20:59:47 +0530

    api projects

---
+++ lorem ipsum generastor/api-key.js
@@ -0,0 +1,2 @@
+//paste your generated api key here
+let apiKey = "S70um7ng1x1J4cHb/H0i1w==2jlik8kDLlAnz1Yy";
---
+++ lorem ipsum generastor/script.js
@@ -0,0 +1,29 @@
+let generateBtn = document.getElementById("generate-btn");
+let copyBtn = document.getElementById("copy-btn");
+let paraDisplay = document.getElementById("para");
+let options = {
+    method: "GET",
+    headers: {"X-api-Key": apiKey },
+}
+
+let url = "https://api.api-ninjas.com/v1/loremipsum?paragraphs=";
+
+let generateParas = () => {
+    let noOfPara = document.getElementById("no-of-para").value;
+    let finalURL = url + noOfPara;
+    fetch(finalURL,options)
+        .then((res) => res.json())
+        .then((data) => {
+            paraDisplay.innerText = data.text;
+        })
+    
+}
+
+//Implement function to copy
+copyBtn.addEventListener("click", () =>{
+    navigator.clipboard.writeText(paraDisplay.innerText);
+    alert("text copied to clipboard")
+})
+
+generateBtn.addEventListener("click", generateParas);
+window.addEventListener("load",generateParas)
